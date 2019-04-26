2d374cc054666b4638fdc66c2a4dbe23ee94ec81
Ajithan <ajithan21@hotmail.com>
2019-04-26 20:38:34 +0530

    created

--- SOSProject/src/connection.php
+++ SOSProject/src/connection.php
@@ -6,15 +6,5 @@ $db_name="sos";
 
 //connection to the database
 $con=mysqli_connect("$hostname", "$username", "$password","$db_name") or die("cannot connect");
-echo "Connected to MySQL<br>";
-
-$result = mysqli_query($con,"SELECT * FROM villages");
-
-//fetch tha data from the database
-while ($row = mysqli_fetch_array($result)) {
-    echo "ID:".$row{'Village_ID'}."<br>";
-}
-//close the connection
-mysqli_close($con);
 
 ?>
