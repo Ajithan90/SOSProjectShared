ae1e6a1baf71f4af43b0f280e98c6f5b649cf46e
Ajithan <ajithan21@hotmail.com>
2019-04-26 19:41:00 +0530

    Connection added

--- SOSProject/src/connection.php
+++ SOSProject/src/connection.php
@@ -1,15 +1,10 @@
 <?php
 $username = "root";
-$password = " ";
+$password = "";
 $hostname = "localhost";
+$db_name="sos";
 
 //connection to the database
-$dbhandle = mysql_connect($hostname, $username, $password)
-or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>";
-
-//select a database to work with
-$selected = mysql_select_db("sos",$dbhandle)
-or die("Could not select examples...");
+$con=mysqli_connect("$hostname", "$username", "$password","$db_name") or die("cannot connect")
 
 ?>
