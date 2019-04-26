df3293f2887a103cd0a8781fa1a0375af59e0883
Ajithan <ajithan21@hotmail.com>
2019-04-26 18:38:09 +0530

    initialcreation

---
+++ SOSProject/src/DashBoard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>DashBoard</title>
+</head>
+    <body>
+    <div>
+    	<a href="Village.php">Village</a>
+    	
+    	</div>
+    </body>
+    
+</html>
