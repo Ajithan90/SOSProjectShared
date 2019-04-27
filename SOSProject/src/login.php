326477e5007aa6b57709979eb030424fdbaee2df
Pirashanthan <Pirashanthan@DESKTOP-R1O1I6H>
2019-04-27 10:33:43 +0530

    login window designed

--- SOSProject/src/login.php
+++ SOSProject/src/login.php
@@ -1,19 +1,47 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
-<title>Insert title here</title>
-</head>
-<frameset>
-    <frame>
-    <frame>
-    <noframes>
-    <body>
-    <p>This page uses frames. The current browser you are using does not support frames.</p>
-    <?php
 
-	?>
+<html>
+
+<head>
+
+<link rel="Stylesheet" type="text/css" href="../css/main.css" >
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+
+
+<title> </title>
+</head>
+
+    <body>
+   <div class="container">
+   <div id="nav-wrapper">
+  <div id="navbar"> <table class="tablewrapper">
+    <tr>
+      <td class="row1">User name</td>
+      <td class="row1">Password</td>
+    </tr>
+
+    <tr>
+      <td> <input type="text" class="inputtext"> </td>
+
+      <td>  <input type="text" class="inputtext">  </td>
+
+      <td>   <div id="button">  Log In   </div>       </td>
+    </tr>
+    
+    <tr>
+      <td> <div class="row2"><input type="checkbox" checked>Keep me logged in</div> </td>
+      
+      <td class="row2 ">Forgot your password?</td>
+  </table>
+    
+    <h1 class="wrapper">  SOS CHILDREN'S VILLAGES  </h1>
+      
+    <h3 class="wrapper">   A loving home for every child  </h3>
+   
+  </div>
+  </div>
+  </div>
+<img src="../images/childern.jpg" width="" height="" />   
     </body>
-    </noframes>
-</frameset>
+   
 </html>
