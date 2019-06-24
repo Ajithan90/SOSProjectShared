4e8c291f823979a118325ede34a6f13eb39c8d81
Ajithan <ajithan21@hotmail.com>
2019-06-24 22:31:31 +0530

    initial creation

--- SOSProject/src/connection.php
+++ SOSProject/src/connection.php
@@ -1,26 +1,12 @@
 
-<?php
+<?php 
 
-class Database extends pdo {
 
-    private $dbtype; 
-    private $host;     
-    private $user;
-    private $pass; 
-    private $database; 
+$con = mysqli_connect("127.0.0.1","root","","sos")
+or die("SERVER Error ".mysqli_error());
+return $con;
 
-    public function __construct(){ 
-        $this->dbtype = 'mysql'; 
-        $this->host = '127.0.0.1'; 
-        $this->user = 'root'; 
-        $this->pass = ''; 
-        $this->database = 'sos'; 
-        $dns = $this->dbtype.':dbname='.$this->database.";host=".$this->host; 
-        parent::__construct( $dns, $this->user, $this->pass ); 
-    }     
-}
 
-$database = new Database();
-$db =& $database;
+
 
 ?>
