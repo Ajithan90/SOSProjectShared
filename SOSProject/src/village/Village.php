65ff35676f8494f095e9962344673b783636c70f
Ajithan <ajithan21@hotmail.com>
2019-07-02 21:24:58 +0530

    added

--- SOSProject/src/village/Village.php
+++ SOSProject/src/village/Village.php
@@ -3,10 +3,10 @@
 <?php 
 session_start();
 
-// Check, if username session is NOT set then this page will jump to login page
-//if ((!isset($_SESSION['user_id']) && (!isset($_SESSION['logd_in'])))) {
-//header('Location:../login.php');
-//}
+ //Check, if username session is NOT set then this page will jump to login page
+if ((!isset($_SESSION['user_id']) && (!isset($_SESSION['logd_in'])))) {
+header('Location:../login.php');
+}
 ?>
 
 </head>
